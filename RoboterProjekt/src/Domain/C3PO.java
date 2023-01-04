89c1be7a33dedc5c1db1b5df284aa51a762ffcef
simon <simon@LAPTOP-CVR0DDE7.speedport.ip>
2023-01-04 21:13:45 +0100

    C3PO Subklasse angelegt

---
+++ RoboterProjekt/src/Domain/C3PO.java
@@ -0,0 +1,62 @@
+package Domain;
+
+import java.util.List;
+
+import Exceptions.RobotException;
+import Exceptions.RobotIllegalStateException;
+import Exceptions.RobotMagicValueException;
+import Domain.Roboter;
+
+public class C3PO extends Roboter {
+
+	/**
+	 * Hier wird der R2D2 durch den Konstruktor erstellt.
+	 * 
+	 * @param name : Hier wird der Name übergeben, wie der Roboter später heißen
+	 *             soll.
+	 */
+	public C3PO(String name) {
+		super(name);
+		super.setId(10000 + (int) Math.random() * 9999 + 1);
+	}
+
+	/**
+	 * Hier denkt der Roboter, also ordnet die Zahlen mithilfe des
+	 * InsertionSort-Algorithmus
+	 *
+	 * @param numbers : Zahlen die sortiert werden sollen
+	 * @return sortierte Zahlen im Integer-Array
+	 * @throws RobotException wenn der Roboter einen ungültigen Zustand oder das
+	 *                        Array nicht passt
+	 */
+	@Override
+	public int[] think(int[] numbers) throws RobotException {
+		int temp;
+		boolean valid = true;
+		for (int number : numbers) {
+			if (number == 42) {
+				valid = false;
+			}
+		}
+		if (super.isPowerOn()) {
+			if (valid == false) {
+				super.getList().add(new RobotMagicValueException(super.getName()));
+				throw new RobotMagicValueException(super.getName());
+			} else {
+				for (int i = 1; i < numbers.length; i++) {
+					temp = numbers[i];
+					int j = i;
+					while (j > 0 && numbers[j - 1] < temp) {
+						numbers[j] = numbers[j - 1];
+						j--;
+					}
+					numbers[j] = temp;
+				}
+				return numbers;
+			}
+		} else {
+			super.getList().add(new RobotIllegalStateException(super.getName()));
+			throw new RobotIllegalStateException(super.getName());
+		}
+	}
+}
