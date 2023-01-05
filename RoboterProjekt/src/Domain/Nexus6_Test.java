bfe331daf262e75aa197f1a3a6776fa978b66ef9
simon <simon@134.155.230.76>
2023-01-05 18:38:21 +0100

    Kommentare + getNexusMethodTest() hinzugefügt zu Nexus6 Testklasse

--- RoboterProjekt/src/Domain/Nexus6_Test.java
+++ RoboterProjekt/src/Domain/Nexus6_Test.java
@@ -11,35 +11,56 @@ import org.junit.jupiter.api.Test;
 class Nexus6_Test {
 	int[] testNumbers = { 1, 3, 2, 4, 8, 7, 6, 5 };
 
+	/**
+	 * Hier wird getestet, ob die statische Methode getNexus() das einzige Nexus6
+	 * Objekt zurückgibt.
+	 */
 	@Test
-	void getTheNexusObjectTest() {
-		assertEquals(Nexus6.nexus,Nexus6.getNexus());
+	void getNexusMethodTest() {
+		assertTrue(Nexus6.getNexus() instanceof Nexus6);
 	}
-	
+
+	/**
+	 * Hier wird getestet, ob die Seriennummer stimmt.
+	 */
 	@Test
 	void serialNumberCheck() {
-		assertEquals(19281982,Nexus6.getNexus().getId());
+		assertEquals(19281982, Nexus6.getNexus().getId());
 	}
-	
+
+	/**
+	 * Hier wird geprüft, ob die Power-Taste funktioniert bzw. defekt ist.
+	 */
 	@Test
 	void PowerSwitchTest() {
 		Nexus6.getNexus().triggerPowerSwitch();
-		assertTrue(Nexus6.getNexus().isPowerOn()==false);
+		assertTrue(Nexus6.getNexus().isPowerOn() == false);
 	}
+
+	/**
+	 * Hier wird geprüft, ob das Nexus6 Objekt den richtigen Namen hat.
+	 */
 	@Test
 	void getNameOfNexusObject() {
-		assertEquals("Pris",Nexus6.getNexus().getName());
+		assertEquals("Pris", Nexus6.getNexus().getName());
 	}
+
+	/**
+	 * Hier wird getestet,ob die think-Mtehode eine Exception wirft.
+	 */
 	@Test
 	void thinkExceptionTest() {
 		Nexus6.getNexus().triggerPowerSwitch();
-		assertThrows(RobotIllegalStateException.class,()->Nexus6.getNexus().think(testNumbers));
+		assertThrows(RobotIllegalStateException.class, () -> Nexus6.getNexus().think(testNumbers));
 	}
+
+	/**
+	 * Hier wird getestet,ob die think-Mtehode eine Exception wirft.
+	 */
 	@Test
 	void speakExceptionTest() {
 		Nexus6.getNexus().triggerPowerSwitch();
-		assertThrows(RobotIllegalStateException.class,()->Nexus6.getNexus().speak(testNumbers));
+		assertThrows(RobotIllegalStateException.class, () -> Nexus6.getNexus().speak(testNumbers));
 	}
-	
 
 }
